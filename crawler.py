865c272f4d79f60b9c7273f3868f060bd1e6399c
Vicente Marçal <vicente.marcal@gmail.com>
2019-10-12 17:24:20 -0400

    Was separated into two files a crawler and a executable
    Close #7

--- crawler.py
+++ crawler.py
@@ -1,5 +1,5 @@
 import math
-from contextlib import closing
+
 import requests
 import json
 import sqlite3
@@ -30,43 +30,3 @@ def get_total_pages(response):
     per_page = response['registrosPorPagina']
     return math.ceil(total / per_page)
 
-
-if __name__ == '__main__':
-
-    total = 1
-    current = 1
-    output = []
-    while current <= total:
-        response = fetch_page(current)
-
-        if current == 1:
-            total = get_total_pages(response)
-
-        for dissertation in response['tesesDissertacoes']:
-            print(dissertation)
-            output.append(dissertation)
-
-        current += 1  # let's go to the next page (the while condition blocks a non-existent page)
-
-    dados = []
-    for saida in output:
-        if saida not in dados:
-            dados.append((saida['id'], saida['instituicao'], saida['nomePrograma'], saida['municipioPrograma'],
-                          saida['titulo'], saida['autor'], saida['dataDefesa'], saida['volumes'], saida['paginas'],
-                          saida['biblioteca'], saida['grauAcademico'], saida['link']))
-
-with sqlite3.connect("tesesDissertacoes.db") as conexao:
-    with closing(conexao.cursor()) as cursor:
-        cursor.execute(""" CREATE TABLE IF NOT EXISTS 
-                           tesesDissertacoes(id integer primary key autoincrement, codigo text,
-                           instituicao text, programa text, municipio text, titulo_trabalho text, autor_trabalho text,
-                           data_defesa text, num_volumes text, num_paginas text, biblioteca text, grau_academico text, 
-                           link text)""")
-
-        cursor.executemany(""" INSERT INTO tesesDissertacoes(codigo, instituicao, programa, municipio, 
-           titulo_trabalho, autor_trabalho, data_defesa, num_volumes, num_paginas, biblioteca, grau_academico, 
-           link) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); """, dados)
-
-        conexao.commit()
-
-        print("Base de dados criada e populada com sucesso!")
